5708c747edf797434c32896741f9443b25c2a4af
Sarah <sarahjeannesexton@live.com>
2016-01-03 22:24:05 -0600

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,8 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res)
-{
-  res.sendFile(__dirname + '/index.html');
-});
+// IMPORTANT LINE: Point to '/public'
+app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
